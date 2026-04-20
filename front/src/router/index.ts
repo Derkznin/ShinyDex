4715bc53a3b98858f0a893dc0ee127a047919455
LRTRC <97665266+LRTRC@users.noreply.github.com>
2026-04-20 15:20:15 +0200

    nettoyage boilerplate

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@/stores/auth.store'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +7,31 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: () => import('../views/HomeView.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/profil',
+      name: 'profil',
+      component: () => import('../views/Profil.vue'),
+      meta: { requiresAuth: true }
     },
+    {
+      path: '/connexion',
+      name: 'connexion',
+      component: () => import('../views/Connexion.vue'),
+    }
   ],
+})
+
+router.beforeEach(async (to, from) => {
+  const authStore = useAuthStore();
+  
+  if (
+    !authStore.isAuthenticated &&
+    to.meta.requiresAuth
+  ) {
+    return { name: 'connexion' }
+  }
 })
 
 export default router
